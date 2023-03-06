efd2ebb8fbeb4179eb14ea786f2b40cdf7620a19
Dex <sheepareawesome777@gmail.com>
2023-03-06 16:17:10 -0500

    Added input maps + Uses vector math for movement
    Former-commit-id: 72a13662424d57809c20053c204eff31b20f0f71

--- pets-gd/scenes/PlayerCB.gd
+++ pets-gd/scenes/PlayerCB.gd
@@ -4,20 +4,7 @@ extends CharacterBody2D
 const SPEED = 80.0
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	######### ^^^^^^^^ IMPORTANT, do this later
-	var dirs_xy = [
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	]
-	
-	for i in range(2):
-		var axis = ["x", "y"][i]
-		
-		if dirs_xy[i]:
-			velocity[axis] = dirs_xy[i] * SPEED
-		else:
-			velocity[axis] = move_toward(velocity[axis], 0, SPEED)
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
 	
 	move_and_slide()
