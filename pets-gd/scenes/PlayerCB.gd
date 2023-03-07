6d7186b02dc74d08ba4ed49357ea5260239afb76
Dex <sheepareawesome777@gmail.com>
2023-03-06 21:46:40 -0500

    Smooth movement

--- pets-gd/scenes/PlayerCB.gd
+++ pets-gd/scenes/PlayerCB.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 
+const ACCELERATION = 12000
+const FRICTION = 20000
+const MAX_SPEED = 10
 
-const SPEED = 360.0
+var vel = Vector2.ZERO
 
 func _physics_process(delta):
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
+	var input_vector = Input.get_vector("left", "right", "up", "down").normalized()
 	
+	if input_vector != Vector2.ZERO:
+		vel += (
+			input_vector *
+			ACCELERATION *
+			delta
+		).clamp(Vector2(-MAX_SPEED, -MAX_SPEED), Vector2(MAX_SPEED, MAX_SPEED))
+	else:
+		vel = vel.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = vel
 	move_and_slide()
