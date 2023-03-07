9ba8119636e09d4a4ddfd617ae5a2317633417a3
Dex <sheepareawesome777@gmail.com>
2023-03-06 22:37:00 -0500

    Godot v4-ify

--- pets-gd/scenes/PlayerCB.gd
+++ pets-gd/scenes/PlayerCB.gd
@@ -1,22 +1,15 @@
 extends CharacterBody2D
 
-const ACCELERATION = 12000
-const FRICTION = 20000
-const MAX_SPEED = 10
-
-var vel = Vector2.ZERO
+const ACCELERATION	= 1600
+const FRICTION		= 5000
+const MAX_SPEED		= 800
 
 func _physics_process(delta):
 	var input_vector = Input.get_vector("left", "right", "up", "down").normalized()
 	
 	if input_vector != Vector2.ZERO:
-		vel += (
-			input_vector *
-			ACCELERATION *
-			delta
-		).clamp(Vector2(-MAX_SPEED, -MAX_SPEED), Vector2(MAX_SPEED, MAX_SPEED))
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
 	else:
-		vel = vel.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 	
-	velocity = vel
 	move_and_slide()
