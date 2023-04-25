6974e65efb3ef64ec3c4b475a619d5a6bfa4eea5
Dex <sheepareawesome777@gmail.com>
2023-04-24 23:50:53 -0400

    Create limitedqueue.gd

---
+++ pets-gd/classes/limitedqueue.gd
@@ -0,0 +1,21 @@
+extends Object
+class_name LimitedQueue
+
+var queue = []
+var capacity = 0;
+
+func _init(l):
+	capacity = l
+	super()
+
+# Used for stuff like last n steps for
+# movement of playable characters
+
+func get_at(i):
+	return queue[i]
+
+func push_front(v):
+	queue.push_front(v)
+	
+	while len(queue) > capacity:
+		queue.pop_back()
