5763da7e167a8ea968f4ded1a7e0d79620fa095b
Dex <sheepareawesome777@gmail.com>
2023-05-13 18:49:43 -0400

    Move anim control stuff into Character (Composition > Inheritance)
    Former-commit-id: e6bb2e03d66861769db064bfe36c947bc83bf18a

--- pets-gd/classes/char.gd
+++ pets-gd/classes/char.gd
@@ -1,4 +1,17 @@
 class_name Character
 extends Node
 
+@onready var anim_player = $AnimationPlayer
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
 
+func _ready():
+	anim_tree.active = true
+
+func anim_move(moving: bool, inputs: Vector2):
+	if moving:
+		anim_tree.set("parameters/Idle/blend_position", inputs)
+		anim_tree.set("parameters/Run/blend_position", inputs)
+		anim_state.travel("Run")
+	else:
+		anim_state.travel("Idle")
