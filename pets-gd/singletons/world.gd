3ceea72c96a4e4814f9fd360755f5c6153d75b07
Dex <sheepareawesome777@gmail.com>
2023-05-14 02:43:27 -0400

    `World` -> `Overworld` (avoid name conflict)

--- pets-gd/singletons/world.gd
+++ pets-gd/singletons/world.gd
@@ -4,3 +4,10 @@ extends Node
 @export var bar = 168
 @export var baz = 0.1
 @export var boing = "hellogus amogus"
+
+const OVERWORLD_NAME := "Overworld"
+
+#func _process(_delta):
+#	var scene := get_tree().current_scene.name
+#	if scene == OVERWORLD_NAME:
+#		pass
