ea0fe37a10df59dc3af7587ffb5a92a2380efc84
Dex <sheepareawesome777@gmail.com>
2023-05-13 20:08:29 -0400

    Add todo/warning about limq performance

--- pets-gd/classes/limitedqueue.gd
+++ pets-gd/classes/limitedqueue.gd
@@ -14,6 +14,9 @@ func _init(l):
 func get_at(i):
 	return queue[i]
 
+# TODO: rewrite in GDExtension when Rust becomes stable
+# Apparently pushing/popping from front is a slow operation for large arrays?
+# https://docs.godotengine.org/en/stable/classes/class_array.html#class-array-method-push-front
 func push_front(v):
 	queue.push_front(v)
 	
