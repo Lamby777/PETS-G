4516ef32d385a291e49eab1c160fef2e13a992a2
Dex <sheepareawesome777@gmail.com>
2023-05-14 03:13:01 -0400

    Add polygon check
    Former-commit-id: 1f3b15a0a08560e859c76c60cea0f81d6e5d614a

--- pets-gd/singletons/world.gd
+++ pets-gd/singletons/world.gd
@@ -1,13 +1,4 @@
+class_name Globals
 extends Node
 
-@export var foo = 192
-@export var bar = 168
-@export var baz = 0.1
-@export var boing = "hellogus amogus"
-
-const OVERWORLD_NAME := "Overworld"
-
-#func _process(_delta):
-#	var scene := get_tree().current_scene.name
-#	if scene == OVERWORLD_NAME:
-#		pass
+@export var world_music	:= ""
