13f2ca607d849f82c69513197135413cbd5de64f
Dex <sheepareawesome777@gmail.com>
2023-05-18 21:55:12 -0400

    Move musiczone code into overworld.gd

--- pets-gd/scenes/overworld.gd
+++ pets-gd/scenes/overworld.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
 @export var world_music	:= ""
+
+@onready var mzones	= $MusicZones
+
+func _ready():
+	check_musiczones()
+
+func check_musiczones():
+	# check if leaving current zone
+	#
+	#
+	#
+	#
+	
+	# check if entering new zone
+	for zone in mzones.get_children():
+		if Geometry2D.is_point_in_polygon(position, zone.polygon):
+			var world_music = zone.get_meta("music")
+			break
