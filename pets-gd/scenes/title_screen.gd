9f3c9ec9ffb9e53755a0aa978a2ae1d877636ba8
Dex <sheepareawesome777@gmail.com>
2023-06-08 10:51:27 -0400

    ColorRect background (`Context` -> `Node2D`)
    Former-commit-id: 639a95d63232f035dfa216c884dcc791873850a0

--- pets-gd/scenes/title_screen.gd
+++ pets-gd/scenes/title_screen.gd
@@ -1,9 +1,10 @@
-extends PanelContainer
+extends Node2D
 
 @onready var start_sound = $StartSound
 
 func _ready():
-	start_sound.play()
+	#start_sound.play()
+	pass
 
 func _process(_delta):
 	pass
