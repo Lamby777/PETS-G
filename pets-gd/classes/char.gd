01f94faa8307171df2205202b3a267e0effdbcd2
Cherry <cherry@sparklet.org>
2023-09-02 01:13:41 -0400

    joining the spaces cult
    Former-commit-id: f70cc82411cbded076f29a9181c8d17c273f7ee2

--- pets-gd/classes/char.gd
+++ pets-gd/classes/char.gd
@@ -1,20 +1,19 @@
 class_name PChar
 extends Node2D
 
-@onready var sprite			= $Sprite2D
-@onready var anim_player	= $AnimationPlayer
-@onready var anim_tree		= $AnimationTree
-@onready var anim_state		= anim_tree["parameters/playback"]
+@onready var sprite       = $Sprite2D
+@onready var anim_player  = $AnimationPlayer
+@onready var anim_tree    = $AnimationTree
+@onready var anim_state   = anim_tree["parameters/playback"]
 
 func _ready():
-	anim_tree.active = true
+  anim_tree.active = true
 
 func anim_move(moving: bool, inputs: Vector2):
-	anim_tree.set("parameters/Idle/blend_position", inputs)
-	anim_tree.set("parameters/Run/blend_position", inputs)
-	
-	if moving:
-		anim_state.travel("Run")
-	else:
-		anim_state.travel("Idle")
-		print(anim_state)
+  anim_tree.set("parameters/Idle/blend_position", inputs)
+  anim_tree.set("parameters/Run/blend_position", inputs)
+
+  if moving:
+    anim_state.travel("Run")
+  else:
+    anim_state.travel("Idle")
