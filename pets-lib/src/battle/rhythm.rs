9eb43336bd2045986aafde0c79bd90fe1793fe47
Cherry <cherry@sparklet.org>
2023-09-04 12:24:52 -0400

    battle engine code restructure + pre adding rhythm stuff
    Former-commit-id: 1f5fecf8f93630f886253436636de3d8b62a213b

---
+++ pets-lib/src/battle/rhythm.rs
@@ -0,0 +1,9 @@
+/*
+ * Rhythms for the battle system...
+ *
+ * Maybe this file could be imported and used for
+ * developing a tool that helps automate the marking process?
+ *
+ * TODO add a way to deserialize from file, to allow for
+ * custom rhythms and modding.
+ */
