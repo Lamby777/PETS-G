4d62b4668bb1bb7adb37a4af1cbf2b5542bca894
Cherry <cherry@sparklet.org>
2023-09-04 12:52:38 -0400

    goofy ahh documentation + added "end" to results list

--- pets-lib/src/dialogue.rs
+++ pets-lib/src/dialogue.rs
@@ -4,12 +4,25 @@
 
 #![allow(dead_code)]
 
+/// Possible outcomes of picking a dialogue option.
+///
+/// Damn, we're really calling it this?
+/// Enum? Option? Result? What do you think this is, Rust?
+/// Oh, wait... it IS Rust...
+///
+/// - Devon, 2037
 enum DialogueOptionResult {
     // can either load to another node
+    // (simple A -> (B or C) type of dialogue)
     NextNode(DialogueNode),
 
     // or could also be a function pointer
+    // (fancy stuff like shops)
     Action(fn()),
+
+    // or just end the tree with that option
+    // and return control to the user ("goodbye")
+    End,
 }
 
 struct DialogueOption {
