22e1f9f41b02adf0a96c060a2a8c9e462832eb92
Cherry <cherry@sparklet.org>
2023-09-04 13:13:28 -0400

    String segments type in dialogue stuff

--- pets-lib/src/dialogue.rs
+++ pets-lib/src/dialogue.rs
@@ -4,6 +4,9 @@
 
 #![allow(dead_code)]
 
+use godot::engine::Font;
+use godot::prelude::*;
+
 /// Possible outcomes of picking a dialogue option.
 ///
 /// Damn, we're really calling it this?
@@ -12,29 +15,40 @@
 ///
 /// - Devon, 2037
 enum DialogueOptionResult {
-    // can either load to another node
-    // (simple A -> (B or C) type of dialogue)
+    /// Leads to another node
+    /// (simple `A -> (B|C)` dialogue)
     NextNode(DialogueNode),
 
-    // or could also be a function pointer
-    // (fancy stuff like shops)
+    /// Leads to running a function pointer
+    /// (fancy stuff like shops)
     Action(fn()),
 
-    // or just end the tree with that option
-    // and return control to the user ("goodbye")
+    /// End the tree with this option and (usually)
+    /// return control to the user ("goodbye")
     End,
 }
 
 struct DialogueOption {
     label: String,
+    available: bool,
     leads_to: DialogueOptionResult,
 }
 
 struct DialogueNode {
-    text: String,
+    text: Vec<StringSegment>,
+    speaker: String,
+
     options: Vec<DialogueOption>,
 }
 
-fn dialogue_yes_no() {
-    unimplemented!();
+/// Part of a string, meant to be joined
+/// with other parts to have partially
+/// formatted sections in a single message
+struct StringSegment {
+    text: String,
+    color: Color,
+    font: Font,
+
+    /// if None, the voice will not be changed
+    vox: Option<String>,
 }
