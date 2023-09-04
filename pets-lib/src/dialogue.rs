494ccbdcecc387235ca875c0ace1fc0e34601b75
Cherry <cherry@sparklet.org>
2023-09-04 12:40:49 -0400

    dialogue options refactor

--- pets-lib/src/dialogue.rs
+++ pets-lib/src/dialogue.rs
@@ -1,8 +1,25 @@
-type DialogueOptions = Vec<DialogueNode>;
+/*
+    Dialogue system for the game.
+*/
+
+#![allow(dead_code)]
+
+enum DialogueOptionResult {
+    // can either load to another node
+    NextNode(Box<DialogueNode>),
+
+    // or could also be a function pointer
+    Action(fn()),
+}
+
+struct DialogueOption {
+    label: String,
+    leads_to: DialogueOptionResult,
+}
 
 struct DialogueNode {
     text: String,
-    options: DialogueOptions,
+    options: Vec<DialogueOption>,
 }
 
 fn dialogue_yes_no() {
