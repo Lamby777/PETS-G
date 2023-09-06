02d5336e80d80f7e6969861e09f3a323325ab27f
Cherry <cherry@sparklet.org>
2023-09-06 13:30:03 -0400

    Track rhythm component enum
    Former-commit-id: bb43fb0a56bc6071e1657f08fa21afc04f190c32

--- pets-lib/src/battle/rhythm.rs
+++ pets-lib/src/battle/rhythm.rs
@@ -7,3 +7,20 @@
 
 // TODO add a way to deserialize from file, to allow for
 // custom rhythms and modding.
+
+//! Rhythms are made up of notes and tempo changes.
+enum RhythmComponent {
+    Note { length: u8 },
+    TempoChange { length: u8, bpm: f64 },
+}
+
+// struct TrackRhythm {
+//     components: Vec<RhythmComponent>,
+// }
+
+const SIMPLE_BEAT: &[RhythmComponent] = &[
+    RhythmComponent::Note { length: 8 },
+    RhythmComponent::Note { length: 8 },
+    RhythmComponent::Note { length: 8 },
+    RhythmComponent::Note { length: 8 },
+];
