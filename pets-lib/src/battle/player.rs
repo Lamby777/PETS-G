9ed82799353dea990c8132dcf677226d4c45e298
Cherry <cherry@sparklet.org>
2023-09-07 12:57:52 -0400

    no more format!() calls every frame (bruh) + friction stuff (incomplete)
    Former-commit-id: ab5adf5d41a9762374a3cdd8749988b6e2aa2afa

--- pets-lib/src/battle/player.rs
+++ pets-lib/src/battle/player.rs
@@ -5,13 +5,28 @@
 use godot::engine::{Node2D, Node2DVirtual};
 use godot::prelude::*;
 
+type DirectionalInputNames = [(&'static str, Vector2); 4];
 const SPEED: f32 = 600.0;
 
-const DIRECTIONS: [(&'static str, Vector2); 4] = [
+const DIRECTIONS: DirectionalInputNames = [
     ("up", Vector2::UP),
     ("down", Vector2::DOWN),
     ("left", Vector2::LEFT),
     ("right", Vector2::RIGHT),
+];
+
+// I spent legit 2 hours trying to find a
+// good way to do this at compile-time without
+// repetition or leaking as static...
+//
+// hopefully this'll be fixed later but it's
+// still better than running format!() once every
+// time process() is called.
+const BATTLE_DIRECTIONS: DirectionalInputNames = [
+    ("battle_move_up", Vector2::UP),
+    ("battle_move_down", Vector2::DOWN),
+    ("battle_move_left", Vector2::LEFT),
+    ("battle_move_right", Vector2::RIGHT),
 ];
 
 #[derive(GodotClass)]
@@ -21,6 +36,7 @@ struct BattleIcon {
     node: Base<Node2D>,
 
     velocity: Vector2,
+    friction: Vector2,
 }
 
 #[godot_api]
@@ -30,19 +46,21 @@ impl Node2DVirtual for BattleIcon {
             node,
 
             velocity: Vector2::new(0.0, 0.0),
+            friction: Vector2::new(0.9, 0.9),
         }
     }
 
     fn process(&mut self, delta: f64) {
         let input = Input::singleton();
 
-        for (dir_s, dir_vec2) in DIRECTIONS {
-            let input_mapping_name = format!("battle_move_{dir_s}",);
-
-            if input.is_action_pressed(input_mapping_name.into()) {
-                self.velocity += dir_vec2;
+        for (k, v) in BATTLE_DIRECTIONS.into_iter() {
+            if input.is_action_pressed(k.into()) {
+                self.velocity += v;
             }
         }
+
+        // Apply friction
+        self.velocity *= self.friction;
 
         if self.velocity.length() > 0.0 {
             self.velocity = self.velocity.normalized() * SPEED;
