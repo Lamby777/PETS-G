9f6391a6f4d6237b20b02b4ab47879e3fcc31702
Cherry <cherry@sparklet.org>
2023-09-07 18:08:57 -0400

    acceleration stuff
    Former-commit-id: 0087db2ecd41b212c6f27351317e1bdd584f7980

--- pets-lib/src/battle/player.rs
+++ pets-lib/src/battle/player.rs
@@ -6,14 +6,6 @@ use godot::engine::{Node2D, Node2DVirtual};
 use godot::prelude::*;
 
 type DirectionalInputNames = [(&'static str, Vector2); 4];
-const SPEED: f32 = 600.0;
-
-const DIRECTIONS: DirectionalInputNames = [
-    ("up", Vector2::UP),
-    ("down", Vector2::DOWN),
-    ("left", Vector2::LEFT),
-    ("right", Vector2::RIGHT),
-];
 
 // I spent legit 2 hours trying to find a
 // good way to do this at compile-time without
@@ -35,8 +27,13 @@ struct BattleIcon {
     #[base]
     node: Base<Node2D>,
 
+    /// Speed of player icon
+    speed: f32,
+    acceleration: f32,
+    friction: f32,
+
+    /// Current velocity of player icon
     velocity: Vector2,
-    friction: Vector2,
 }
 
 #[godot_api]
@@ -45,29 +42,31 @@ impl Node2DVirtual for BattleIcon {
         Self {
             node,
 
+            speed: 120.0,
+            acceleration: 0.2,
+            friction: 0.2,
+
             velocity: Vector2::new(0.0, 0.0),
-            friction: Vector2::new(0.9, 0.9),
         }
     }
 
     fn process(&mut self, delta: f64) {
         let input = Input::singleton();
+        let mut accel_amt = Vector2::new(0.0, 0.0);
+
+        self.velocity *= self.friction;
 
         for (k, v) in BATTLE_DIRECTIONS.into_iter() {
             if input.is_action_pressed(k.into()) {
-                self.velocity += v;
+                accel_amt += v * self.acceleration;
             }
         }
 
-        // Apply friction
-        self.velocity *= self.friction;
-
-        if self.velocity.length() > 0.0 {
-            self.velocity = self.velocity.normalized() * SPEED;
-        }
+        // if self.velocity.length() > 0.0 {
+        self.velocity += accel_amt;
+        self.velocity = self.velocity.normalized() * self.speed;
 
         let change = self.velocity * real::from_f64(delta);
-
         let position = self.node.get_global_position() + change;
         let position = Vector2::new(position.x.clamp(0.0, 1920.0), position.y.clamp(0.0, 1080.0));
         self.node.set_global_position(position);
