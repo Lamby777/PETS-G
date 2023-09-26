775eab5b2907e3e7452ba838d0839c09a785d0e9
Cherry <cherry@sparklet.org>
2023-09-26 15:51:48 -0400

    TODO about stat graph (sync)

--- pets-lib/src/battle/stat_translation.rs
+++ pets-lib/src/battle/stat_translation.rs
@@ -8,6 +8,10 @@ pub mod to_battle {
     use crate::prelude::*;
 
     pub fn speed(speed: IntegralStat) -> FloatStat {
+        // oh boy, linear algebra! maybe school DID actually teach me something
+        // TODO maybe use logarithms, refer to that one graph i made a while ago
+        // ofc,, past me didn't know about the "units" in godot, so values might
+        // need to be adjusted for speed and similar implementation-heavy stats
         ((speed as FloatStat) * SPEED_STAT_MULTIPLIER) + SPEED_STAT_Y_INT
     }
 }
