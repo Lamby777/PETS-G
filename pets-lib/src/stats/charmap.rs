9105a27c0f7f0a02a6af7731554adbb83536671c
Cherry <cherry@sparklet.org>
2023-09-30 03:28:27 -0400

    better ch_unique macro
    Former-commit-id: 55ba8d0f9215de1da53d20799471668399e16c84

--- pets-lib/src/stats/charmap.rs
+++ pets-lib/src/stats/charmap.rs
@@ -5,11 +5,10 @@ use crate::prelude::*;
 /// CharMap with all characters having the same exact stats
 pub fn uniform_charmap() -> CharMap {
     let mut res = CharMap::new();
-    let deft_stats = CharData::default();
 
     // add chars from registry
     for chname in PChar::ALL.iter() {
-        let cloned = deft_stats.clone();
+        let cloned = CharData::default();
         res.insert(chname.to_string(), Rc::new(RefCell::new(cloned)));
     }
 
@@ -19,11 +18,16 @@ pub fn uniform_charmap() -> CharMap {
 /// "Jat Chippity goes hard"
 /// Makes it easier to write custom base stats and stuff
 macro_rules! ch_unique {
-    ($map:expr, $character:expr, $($field:ident $(.$property:ident)? = $value:expr),*) => {
-        $map.entry($character.to_owned()).and_modify(|pchar| {
-            let mut pchar = pchar.borrow_mut();
-            $(pchar.$field$(.$property)? = $value;)*
-        });
+    ($map:expr, $($character:ident {$($field:ident $(.$property:ident)? = $value:expr),*}),*) => {
+        $(
+            let character = PChar::$character;
+            $map.entry(character.to_owned()).and_modify(|pchar| {
+                let mut pchar = pchar.borrow_mut();
+                $(
+                    pchar.$field$(.$property)? = $value;
+                )*
+            });
+        )*
     };
 }
 
@@ -32,22 +36,71 @@ macro_rules! ch_unique {
 pub fn default_charmap() -> CharMap {
     let mut res = uniform_charmap();
 
-    // Ethan's special stuff
-    ch_unique!(
-        res,
-        PChar::ETHAN,
-        name = "Ethan".to_string(),
-        base_stats.lambda = Some(1),
-        base_stats.max_mana = Some(1)
-    );
+    // max_hp, max_energy, attack, defense, speed,
+    // stability, delta, epsilon, lambda, max_mana,
 
-    // Siva's special stuff
-    ch_unique!(
+    ch_unique! {
         res,
-        PChar::SIVA,
-        name = "Siva".to_string(),
-        base_stats.max_mana = Some(1)
-    );
+
+        ETHAN {
+            display_name = "Ethan".to_string(),
+            base_stats.max_hp = 12,
+            base_stats.lambda = Some(1),
+            base_stats.max_mana = Some(1)
+        },
+
+        SIVA {
+            display_name = "Siva".to_string(),
+            base_stats.max_hp = 18,
+            base_stats.max_mana = Some(1)
+        },
+
+        TERRA {
+            display_name = "Terra".to_string(),
+            base_stats.max_hp = 26
+        }
+    }
 
     res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ch_unique_macro_works() {
+        let mut charmap = uniform_charmap();
+
+        ch_unique! {
+            charmap,
+
+            ETHAN {
+                display_name = "Ethan".to_string(),
+                base_stats.max_hp = 12,
+                base_stats.lambda = Some(1),
+                base_stats.max_mana = Some(1)
+            },
+
+            SIVA {
+                display_name = "Siva".to_string(),
+                base_stats.max_hp = 18,
+                base_stats.max_mana = Some(1)
+            }
+        }
+
+        let ethan = charmap.get(&PChar::ETHAN.to_string()).unwrap();
+        let ethan = ethan.borrow();
+        assert_eq!(ethan.display_name, "Ethan");
+        assert_eq!(ethan.base_stats.max_hp, 12);
+        assert_eq!(ethan.base_stats.lambda, Some(1));
+        assert_eq!(ethan.base_stats.max_mana, Some(1));
+
+        let siva = charmap.get(&PChar::SIVA.to_string()).unwrap();
+        let siva = siva.borrow();
+        assert_eq!(siva.display_name, "Siva");
+        assert_eq!(siva.base_stats.max_hp, 18);
+        assert_eq!(siva.base_stats.lambda, None);
+        assert_eq!(siva.base_stats.max_mana, Some(1));
+    }
 }
