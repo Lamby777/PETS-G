1914315c2e2584ff33b28dbc6acc1888a995624a
Cherry <cherry@sparklet.org>
2023-09-30 17:45:31 -0400

    remove the Rc stuff from last night (forgot what i was doing but i need to commit this before the next change...
    Former-commit-id: 9269355930fea499e754035e419f68e8a973091c

--- pets-lib/src/stats/statcalc.rs
+++ pets-lib/src/stats/statcalc.rs
@@ -1,13 +1,13 @@
 /// Function that calculates a stat for a character
 /// Basically just "base stats" as a function
 use super::IntegralStat;
-use std::{collections::HashMap, rc::Rc};
+use std::collections::HashMap;
 
 // function that returns the same type it takes in
 pub type StatCalcFn<T> = Option<fn(T) -> T>;
 
 // no refcell necessary because they're just functions
-pub type CharStatCalcs = HashMap<String, Rc<StatCalcList>>;
+pub type CharStatCalcs = HashMap<String, StatCalcList>;
 
 /// A list of stat calculation functions for ONE CHARACTER
 #[derive(Default, Debug, Clone)]
@@ -18,72 +18,11 @@ pub struct StatCalcList {
     pub attack: StatCalcFn<IntegralStat>,
     pub defense: StatCalcFn<IntegralStat>,
     pub speed: StatCalcFn<IntegralStat>,
-    pub stability: StatCalcFn<IntegralStat>, // PK defense
+    pub stability: StatCalcFn<IntegralStat>,
 
-    // refer to google doc for what these do...
-    // can't pick a good name for em yet
-    pub delta: StatCalcFn<IntegralStat>,   // "the crit one"
-    pub epsilon: StatCalcFn<IntegralStat>, // "the combo one"
+    pub delta: StatCalcFn<IntegralStat>,
+    pub epsilon: StatCalcFn<IntegralStat>,
 
-    // Exclusive to certain characters
-    // NOTE maybe use traits for this?
-    // idk the overhead of dynamic dispatch might not be worth it
     pub lambda: StatCalcFn<Option<IntegralStat>>,
     pub max_mana: StatCalcFn<Option<IntegralStat>>,
-}
-
-macro_rules! use_standard {
-    ($($stat:ident),*) => {
-        Self {
-            $($stat: Some(standard_calcs::$stat)),*
-        }
-    };
-}
-
-impl StatCalcList {
-    /// Not to be confused with `default()` from the
-    /// `Default` trait, this function returns a calc list
-    /// that should be referenced whenever a character's
-    /// calc list shows "None"
-    pub fn standard() -> Self {
-        use_standard! {
-            max_hp,
-            max_energy,
-            attack,
-            defense,
-            speed,
-            stability,
-            delta,
-            epsilon,
-            lambda,
-            max_mana
-        }
-    }
-}
-
-mod standard_calcs {
-    use crate::prelude::IntegralStat;
-
-    macro_rules! identities {
-        ($($ty:ty | $name:ident),*) => {
-            $(
-                pub fn $name(x: $ty) -> $ty {
-                    x
-                }
-            )*
-        };
-    }
-
-    identities! {
-        IntegralStat | max_hp,
-        IntegralStat | max_energy,
-        IntegralStat | attack,
-        IntegralStat | defense,
-        IntegralStat | speed,
-        IntegralStat | stability,
-        IntegralStat | delta,
-        IntegralStat | epsilon,
-        Option<IntegralStat> | lambda,
-        Option<IntegralStat> | max_mana
-    }
 }
