43bdcdbeae2c6bd128211e4a328cb0c6a0959c65
Cherry <cherry@sparklet.org>
2023-09-30 02:57:30 -0400

    Added `ch_unique` macro for editing default charmap

--- pets-lib/src/stats/charmap.rs
+++ pets-lib/src/stats/charmap.rs
@@ -16,23 +16,38 @@ pub fn uniform_charmap() -> CharMap {
     res
 }
 
+/// "Jat Chippity goes hard"
+/// Makes it easier to write custom base stats and stuff
+macro_rules! ch_unique {
+    ($map:expr, $character:expr, $($field:ident $(.$property:ident)? = $value:expr),*) => {
+        $map.entry($character.to_owned()).and_modify(|pchar| {
+            let mut pchar = pchar.borrow_mut();
+            $(pchar.$field$(.$property)? = $value;)*
+        });
+    };
+}
+
 /// CharMap at the start of the game
 /// Most characters have unique base stats
 pub fn default_charmap() -> CharMap {
     let mut res = uniform_charmap();
 
     // Ethan's special stuff
-    res.entry(PChar::ETHAN.to_owned()).and_modify(|pchar| {
-        let mut pchar = pchar.borrow_mut();
-        pchar.base_stats.lambda = Some(1);
-        pchar.base_stats.max_mana = Some(20);
-    });
+    ch_unique!(
+        res,
+        PChar::ETHAN,
+        name = "Ethan".to_string(),
+        base_stats.lambda = Some(1),
+        base_stats.max_mana = Some(1)
+    );
 
     // Siva's special stuff
-    res.entry(PChar::SIVA.to_owned()).and_modify(|pchar| {
-        let mut pchar = pchar.borrow_mut();
-        pchar.base_stats.max_mana = Some(20);
-    });
+    ch_unique!(
+        res,
+        PChar::SIVA,
+        name = "Siva".to_string(),
+        base_stats.max_mana = Some(1)
+    );
 
     res
 }
