155de5860433c63dba204331bc449bd1b2e702f2
Cherry <cherry@sparklet.org>
2023-10-23 00:31:16 -0400

    pchar node impl finished (not tested)
    Former-commit-id: f96186649eb35d6bb2f6f4691159a32e9fcbdfda

--- pets-lib/src/world/pchar_node.rs
+++ pets-lib/src/world/pchar_node.rs
@@ -1,19 +1,8 @@
-// @onready var anim_state   = anim_tree["parameters/playback"]
-//
-// func anim_move(moving: bool, inputs: Vector2):
-//   anim_tree.set("parameters/Idle/blend_position", inputs)
-//   anim_tree.set("parameters/Run/blend_position", inputs)
-//
-//   if moving:
-//     anim_state.travel("Run")
-//   else:
-//     anim_state.travel("Idle")
-//
-
-use godot::engine::{AnimationPlayer, AnimationTree, Sprite2D};
+use godot::engine::AnimationNodeStateMachinePlayback;
+use godot::engine::AnimationPlayer;
+use godot::engine::AnimationTree;
+use godot::engine::Sprite2D;
 use godot::prelude::*;
-
-use crate::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Node2D)]
@@ -24,13 +13,28 @@ pub struct PCharNode {
     sprite: Gd<Sprite2D>,
     anim_player: Gd<AnimationPlayer>,
     anim_tree: Gd<AnimationTree>,
+    anim_state: Gd<AnimationNodeStateMachinePlayback>,
+}
+
+#[godot_api]
+impl PCharNode {
+    fn anim_move(&mut self, moving: bool, inputs: Vector2) {
+        let mode_str = if moving { "Run" } else { "Idle" };
+        let anim_path = format!("parameters/{mode_str}/blend_position");
+
+        self.anim_tree.set(anim_path.into(), Variant::from(inputs));
+        self.anim_state.travel(mode_str.into());
+    }
 }
 
 #[godot_api]
 impl Node2DVirtual for PCharNode {
     fn ready(&mut self) {
         self.sprite = self.node.get_node_as("Sprite2D");
+        self.anim_player = self.node.get_node_as("AnimationPlayer");
+        self.anim_tree = self.node.get_node_as("AnimationTree");
+        self.anim_state = self.anim_tree.get("parameters/playback".into()).to();
 
-        self.anim_tree.active = true;
+        self.anim_tree.set_active(true);
     }
 }
