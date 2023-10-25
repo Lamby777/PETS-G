200d0ae69814a7f6aad924b2439aa0e3156547ef
Cherry <cherry@sparklet.org>
2023-10-25 13:25:19 -0400

    split interaction module into zone and manager
    Former-commit-id: aa25bfd8b50d9e093c7b8a75ab66a0058c6d6cdd

---
+++ pets-lib/src/world/interaction/zone.rs
@@ -0,0 +1,33 @@
+//!
+//! Area that allows the player to start
+//! an interaction when within range
+//!
+
+use godot::prelude::*;
+
+use godot::engine::{Area2D, Area2DVirtual};
+
+#[derive(GodotClass)]
+#[class(base=Area2D)]
+struct InteractionZone {
+    #[base]
+    node: Base<Area2D>,
+
+    #[export]
+    name: GodotString,
+}
+
+#[godot_api]
+impl InteractionZone {}
+
+#[godot_api]
+impl Area2DVirtual for InteractionZone {
+    fn init(node: Base<Area2D>) -> Self {
+        Self {
+            node,
+            name: "".into(),
+        }
+    }
+
+    // fn ready(&mut self) {}
+}
