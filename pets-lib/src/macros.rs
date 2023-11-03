9f8d49374fb74b8024bfb449c3b9a1e036b643d1
Cherry <cherry@sparklet.org>
2023-11-02 23:55:04 -0400

    `node_at` macro
    Former-commit-id: ad4ca7a842d2e28c0f2e83df1d3ba78e4d76e4d1

--- pets-lib/src/macros.rs
+++ pets-lib/src/macros.rs
@@ -8,3 +8,27 @@ macro_rules! uninit {
         }
     };
 }
+
+/// Gets the node at a given path, without having to pass in self.node
+#[macro_export]
+macro_rules! node_at {
+    ($path:expr) => {
+        godot::engine::Engine::singleton()
+            .get_main_loop()
+            .unwrap()
+            .cast::<SceneTree>()
+            .get_root()
+            .unwrap()
+            .get_node_as($path)
+    };
+
+    ($path:expr, $type:ty) => {
+        godot::engine::Engine::singleton()
+            .get_main_loop()
+            .unwrap()
+            .cast::<SceneTree>()
+            .get_root()
+            .unwrap()
+            .get_node_as::<$type>($path)
+    };
+}
