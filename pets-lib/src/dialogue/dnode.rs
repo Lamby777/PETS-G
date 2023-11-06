ad3d1894a9fac06c63411a2b4c0263c41286b784
Cherry <cherry@sparklet.org>
2023-11-06 13:27:21 -0500

    dialogue node types stuff + test

--- pets-lib/src/dialogue/dnode.rs
+++ pets-lib/src/dialogue/dnode.rs
@@ -3,35 +3,68 @@
 //! for the actual dialogue text, choices, etc.
 //!
 
+use crate::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+pub struct DialogueNode {
+    // TODO: static string
+    id: String,
+
+    // TODO replace with &'a str
+    speaker: String,
+    vox: String,
+
+    text: String,
+    options: Option<Vec<DialogueChoice>>,
+}
+
 /// Possible outcomes of picking a dialogue option.
 ///
 /// "Yeah, this new name's WAY less confusing... right?"
 /// - Devon, 2037
+#[derive(Serialize, Deserialize)]
 pub enum DialogueAction {
     /// Leads to another node
     /// (simple `A -> (B|C)` dialogue)
     NextNode(DialogueNode),
 
-    /// Leads to running a function pointer
+    /// Leads to running a function
     /// (fancy stuff like shops)
-    Action(fn()),
+    /// TODO static string
+    Action(String),
 
     /// End the tree with this option and (usually)
     /// return control to the user ("goodbye")
     End,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct DialogueChoice {
+    /// the text saying what the choice is
     label: String,
+
+    /// whether the choice is selectable or grayed out
+    /// TODO: maybe should be separate from this struct?
     available: bool,
-    leads_to: DialogueAction,
+
+    /// the action this choice leads to
+    action: DialogueAction,
 }
 
-pub struct DialogueNode {
-    // TODO replace with &'a str
-    speaker: String,
-    vox: String,
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    text: String,
-    options: Vec<DialogueChoice>,
+    // TODO test for options
+
+    #[test]
+    fn dnode_fields() {
+        let dnode = DialogueNode {
+            id: "test_interaction".to_string(),
+            speaker: "Cherry".to_string(),
+            vox: "Mira".to_string(),
+            text: "This dialogue is for testing purposes".to_string(),
+            options: None,
+        };
+    }
 }
