b67b396de89573a93e09c4f4d15400ac83c8da72
Cherry <cherry@sparklet.org>
2023-11-23 20:42:50 -0500

    add build script for dialogue compilation

---
+++ pets-lib/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=dg/src/");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dir = PathBuf::from(&dir);
+
+    let dg_dir = dir.join("dg");
+
+    Command::new("dg")
+        .args(&["src/main.dg", "-o", "packed.dgc"])
+        .current_dir(&dg_dir)
+        .status()
+        .unwrap();
+}
