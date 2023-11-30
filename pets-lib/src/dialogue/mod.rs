36d0e0f7a96b20e29b463b6975c654de209f49a5
Cherry <cherry@sparklet.org>
2023-11-29 20:21:47 -0500

    load `packed.dgc` into a `OnceLock` with pub function to access or init

--- pets-lib/src/dialogue/mod.rs
+++ pets-lib/src/dialogue/mod.rs
@@ -4,3 +4,32 @@
 
 pub mod autoload;
 pub mod dbox;
+
+use dialogical::prelude::*;
+use indoc::indoc;
+
+use std::sync::OnceLock;
+
+static INTERACTIONS: OnceLock<Vec<Interaction>> = OnceLock::new();
+
+macro_rules! packed_dialogue {
+    () => {
+        dialogical::deserialize(include_bytes!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/dg/packed.dgc"
+        )))
+    };
+}
+
+// load dialogues
+pub fn interactions() {
+    INTERACTIONS.get_or_init(|| {
+        packed_dialogue!().expect(indoc! {"
+            Failed to load dialogues. If you are a player,
+            please report this to the developers. If you're
+            a contributor, make sure the build script has
+            ran properly or manually compile the interaction
+            list yourself.
+        "})
+    });
+}
