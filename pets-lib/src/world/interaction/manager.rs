bbdd5edbe4e1e8f86c3a27b55cc460c5c3ed55b4
Cherry <cherry@sparklet.org>
2023-11-30 20:51:28 -0500

    return early and don't allow iz interact if a dialog is currently active

--- pets-lib/src/world/interaction/manager.rs
+++ pets-lib/src/world/interaction/manager.rs
@@ -108,6 +108,11 @@ impl INode2D for InteractionManager {
 
     fn input(&mut self, event: Gd<InputEvent>) {
         if event.is_action_pressed("ui_accept".into()) {
+            let di = DBoxInterface::singleton();
+            if di.bind().scene_has_active_dbox() {
+                return;
+            }
+
             self.closest_zone().bind().interact();
         }
     }
