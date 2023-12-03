acb3178c62ea5e2647a29fd5e75025e0c47a1c91
Cherry <cherry@sparklet.org>
2023-12-03 18:10:15 -0500

    create and use `tween` function instead of spamming tween boilerplate everywhere

--- pets-lib/src/consts.rs
+++ pets-lib/src/consts.rs
@@ -24,7 +24,7 @@ pub mod dialogue {
     pub const DBOX_NODE_NAME: &str = "DialogBox";
     pub const DBOX_TWEEN_TIME: f64 = 0.5;
     pub const DBOX_TWEEN_TRANS: TransitionType = TransitionType::TRANS_QUAD;
-    pub const DBOX_CHOICE_HEIGHT: f32 = 100.0;
+    pub const DBOX_CHOICE_HEIGHT: f32 = 60.0;
     pub const DBOX_CHOICE_WAVE_TIME: f64 = 0.1;
 }
 
--- pets-lib/src/util.rs
+++ pets-lib/src/util.rs
@@ -1,11 +1,47 @@
 //!
-//! Macros for mostly general-purpose Godot/Rust stuff
+//! General-purpose Godot/Rust helper stuff
 //!
+
+use godot::engine::tween::TransitionType;
+use godot::engine::Tween;
+use godot::prelude::*;
 
 pub use crate::current_scene;
 pub use crate::godot_root;
 pub use crate::godot_tree;
 pub use crate::node_at;
+
+/// shorthand to do some tweeneroonies :3
+#[must_use = "`None` = failed to create tween"]
+pub fn tween<NP, V>(
+    mut node: Gd<Node>,
+    property: NP,
+    from: Option<V>,
+    target: V,
+    time: f64,
+    trans: TransitionType,
+) -> Option<Gd<Tween>>
+where
+    NP: Into<NodePath>,
+    V: ToGodot,
+{
+    let mut tween = node.create_tween()?;
+
+    let mut property = tween
+        .tween_property(
+            node.clone().upcast(),
+            property.into(),
+            target.to_variant(),
+            time,
+        )?
+        .set_trans(trans)?;
+
+    if let Some(from) = from {
+        property.from(from.to_variant())?;
+    }
+
+    Some(tween)
+}
 
 #[macro_export]
 macro_rules! uninit {
