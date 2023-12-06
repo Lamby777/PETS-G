5c62c6c358da4a0ac20f826a83239ec020d14f69
Cherry <cherry@sparklet.org>
2023-12-05 22:03:41 -0500

    erm ackshually i "rewrote it in rust" 🤡

--- pets-gd/scenes/dialogchoicecontainer.gd
+++
@@ -1,8 +0,0 @@
-class_name DChoiceContainer
-extends Container
-
-func _ready():
-  pass
-
-func _process(_delta):
-  pass
---
+++ pets-lib/src/dialogue/dbox/dchoice.rs
@@ -0,0 +1,29 @@
+//!
+//! custom class for choice labels
+//!
+
+use godot::engine::notify::ContainerNotification;
+use godot::engine::{Container, Control, IContainer};
+use godot::prelude::*;
+
+#[derive(GodotClass)]
+#[class(base=Container)]
+pub struct DChoice {
+    #[base]
+    node: Base<Container>,
+}
+
+#[godot_api]
+impl IContainer for DChoice {
+    fn on_notification(&mut self, what: ContainerNotification) {
+        let node = &mut self.node;
+
+        if what == ContainerNotification::SortChildren {
+            for c in node.get_children().iter_shared() {
+                let c = c.cast::<Control>();
+                let size = node.get_size();
+                node.fit_child_in_rect(c, Rect2::new(Vector2::ZERO, size));
+            }
+        }
+    }
+}
