3fc2cc78a11d7d8de0f461390f85f1880220dd97
Cherry <cherry@sparklet.org>
2023-12-10 00:03:07 -0500

    "the dang chemicals in the game engine are turning the shaders gay"

--- pets-gd/singletons/globals.gd
+++ pets-gd/singletons/globals.gd
@@ -1,14 +1,9 @@
 class_name Globals
 extends Node
-
-func _process(_delta):
-  pass
   
 func _input(_event):
   if Input.is_key_pressed(KEY_0):
     get_tree().change_scene_to_file("res://scenes/world.tscn")
-  #elif Input.is_key_pressed(KEY_1):
-    #get_tree().change_scene_to_file("res://scenes/dialog.tscn")
   elif Input.is_key_pressed(KEY_Q):
     get_tree().quit()
 
