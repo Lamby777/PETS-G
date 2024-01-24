df77fb4cf738751f6106a3a8b42c930e44437cf2
Cherry <cherry@sparklet.org>
2024-01-24 09:56:44 -0500

    (un)register autoloads using `libdx::foreach_static!`

--- pets-lib/src/singleton.rs
+++ pets-lib/src/singleton.rs
@@ -1,11 +1,12 @@
 use godot::engine::Engine;
+use godot::obj::NewAlloc;
 use godot::prelude::*;
 
 ///
 /// Trait for autoloaded classes
 /// Makes it more convenient to get the singleton instance
 ///
-pub trait Autoload: GodotClass + Inherits<Object> {
+pub trait Autoload: GodotClass + Inherits<Object> + NewAlloc {
     const AUTOLOAD_NAME: &'static str;
 
     /// Get a shared ref to use in other nodes
@@ -14,5 +15,17 @@ pub trait Autoload: GodotClass + Inherits<Object> {
             .get_singleton(Self::AUTOLOAD_NAME.into())
             .unwrap()
             .cast()
+    }
+
+    /// Register the singleton with the engine
+    fn register() {
+        let gd = Self::new_alloc().upcast();
+        let name = Self::AUTOLOAD_NAME.into();
+        Engine::singleton().register_singleton(name, gd);
+    }
+
+    fn unregister() {
+        let name = Self::AUTOLOAD_NAME.into();
+        Engine::singleton().unregister_singleton(name);
     }
 }
