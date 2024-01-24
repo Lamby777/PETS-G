07655a4e7bb7b9e357b1851f1b69d2fb2eaf7805
Cherry <cherry@sparklet.org>
2024-01-24 15:26:41 -0500

    actually change scenes in `debug_battle` fn

--- pets-lib/src/functions.rs
+++ pets-lib/src/functions.rs
@@ -14,13 +14,20 @@ impl Autoload for FnInterface {
 // Because I'm totally not gonna regret this later
 #[godot_api]
 impl FnInterface {
+    /// shorthand for `callv` with no args
     pub fn call(name: &str) {
+        Self::callv(name, VariantArray::new())
+    }
+
+    /// Call a global function by name with args
+    pub fn callv(name: &str, args: VariantArray) {
         let callable = Self::singleton().callable(name);
-        callable.callv(Array::new());
+        callable.callv(args);
     }
 
     #[func]
     pub fn debug_battle() {
-        godot_print!("hey :D");
+        // TODO rename `battle_engine` to `battle`
+        change_scene!("battle_engine");
     }
 }
