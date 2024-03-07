5a8aa8e93e61340719cea809f9c57edf1406bae1
Cherry <cherry@sparklet.org>
2024-03-07 08:37:17 -0500

    add `flipped` arg in enemy anim player

--- pets-lib/src/world/enemy_node.rs
+++ pets-lib/src/world/enemy_node.rs
@@ -10,12 +10,29 @@ pub struct WalkingEnemy {
 
 #[godot_api]
 impl WalkingEnemy {
+    /// Set the animation of the enemy
+    ///
+    /// # Arguments
+    ///
+    /// * `moving` - If true, plays running anims, else idle.
+    /// * `flipped` - If true, flips the sprite horizontally.
+    /// * `backwards` - If true, play backwards-facing sprites.
+    ///
+    /// The running sprites are drawn by default facing left.
+    /// `flipped` should be true if the enemy is facing right.
+    ///
+    /// Backwards should be used if the enemy is below the player
+    /// in terms of Y position, so they'd be running up the screen.
     #[func]
-    pub fn anim_move(&mut self, moving: bool, backwards: bool) {
-        let _mode_str = if moving { "Run" } else { "Idle" };
-        let _dir_str = if backwards { "Back" } else { "" };
+    pub fn anim_move(&mut self, moving: bool, flipped: bool, _backwards: bool) {
+        let mode_str = if moving { "Run" } else { "Idle" };
 
-        // format!("{mode_str}{dir_str}");
+        // TODO uncomment when backwards sprites are added
+        // let dir_str = if backwards { "Back" } else { "" };
+        // let anim_name = format!("{mode_str}{dir_str}");
+
+        self.sprite.set_animation(mode_str.into());
+        self.sprite.set_flip_h(flipped);
     }
 }
 
