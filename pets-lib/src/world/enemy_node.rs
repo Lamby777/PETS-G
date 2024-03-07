97de1d9b44b7b178f5dae686e42779231f8a9fb1
Cherry <cherry@sparklet.org>
2024-03-06 20:06:52 -0500

    made it into a basic AnimatedSprite2D and rm the animplayer/tree crap

--- pets-lib/src/world/enemy_node.rs
+++ pets-lib/src/world/enemy_node.rs
@@ -1,28 +1,21 @@
-use godot::engine::{
-    AnimationNodeStateMachinePlayback, AnimationPlayer, AnimationTree, IStaticBody2D, Sprite2D,
-    StaticBody2D,
-};
+use godot::engine::{AnimatedSprite2D, IStaticBody2D, StaticBody2D};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=StaticBody2D)]
 pub struct WalkingEnemy {
     base: Base<StaticBody2D>,
-    sprite: OnReady<Gd<Sprite2D>>,
-    anim_player: OnReady<Gd<AnimationPlayer>>,
-    anim_tree: OnReady<Gd<AnimationTree>>,
-    anim_state: OnReady<Gd<AnimationNodeStateMachinePlayback>>,
+    sprite: OnReady<Gd<AnimatedSprite2D>>,
 }
 
 #[godot_api]
 impl WalkingEnemy {
     #[func]
-    pub fn anim_move(&mut self, moving: bool, inputs: Vector2) {
+    pub fn anim_move(&mut self, moving: bool, backwards: bool) {
         let mode_str = if moving { "Run" } else { "Idle" };
-        let anim_path = format!("parameters/{mode_str}/blend_position");
+        let dir_str = if backwards { "Back" } else { "" };
 
-        self.anim_tree.set(anim_path.into(), Variant::from(inputs));
-        self.anim_state.travel(mode_str.into());
+        // format!("{mode_str}{dir_str}");
     }
 }
 
@@ -32,24 +25,12 @@ impl IStaticBody2D for WalkingEnemy {
         Self {
             base,
             sprite: OnReady::manual(),
-            anim_player: OnReady::manual(),
-            anim_tree: OnReady::manual(),
-            anim_state: OnReady::manual(),
         }
     }
 
     fn ready(&mut self) {
-        let sprite = self.base().get_node_as("Sprite2D");
-        let anim_player = self.base().get_node_as("AnimationPlayer");
+        let sprite = self.base().get_node_as("AnimatedSprite2D");
         self.sprite.init(sprite);
-        self.anim_player.init(anim_player);
-
-        let mut tree = self.base().get_node_as::<AnimationTree>("AnimationTree");
-        tree.set_active(true);
-        let anim_state = tree.get("parameters/playback".into()).to();
-        self.anim_state.init(anim_state);
-
-        self.anim_tree.init(tree);
     }
 
     fn physics_process(&mut self, _delta: f64) {
