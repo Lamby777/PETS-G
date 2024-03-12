5fff87a153285d39a4d933f99e35c9fa07d8bc62
Cherry <cherry@sparklet.org>
2024-03-12 01:17:26 -0400

    split battler trait to another file

--- pets-lib/src/stats/mod.rs
+++ pets-lib/src/stats/mod.rs
@@ -6,6 +6,7 @@ use crate::prelude::*;
 
 // stat-related submodules
 pub mod autoload;
+pub mod battler;
 pub mod charmap;
 pub mod pchars;
 pub mod savefiles;
@@ -20,45 +21,6 @@ pub use statcalc::{CharStatCalcs, StatCalcFn, StatCalcList};
 pub type CharMap = HashMap<String, RefCell<CharData>>;
 pub type IntegralStat = i16;
 pub type FloatStat = f32;
-
-/// Trait for stuff that both party members and enemies
-/// have. For example, an enemy doesn't need to have a
-/// "level," but it does need to have HP and status effects.
-trait Battler {
-    fn hp_mut(&mut self) -> &mut IntegralStat;
-    fn max_hp(&self) -> IntegralStat;
-    fn status_effects(&self) -> &HashSet<StatusEffect>;
-    fn status_effects_mut(&mut self) -> &mut HashSet<StatusEffect>;
-
-    /// Subtract damage count from the character's HP
-    ///
-    /// Saturated at 0.
-    fn take_damage(&mut self, damage: IntegralStat) {
-        let hp = *self.hp_mut();
-        *self.hp_mut() = 0.max(hp - damage);
-    }
-
-    /// Add back HP to the character
-    ///
-    /// Saturated at the character's max HP
-    fn heal(&mut self, amount: IntegralStat) {
-        let hp = *self.hp_mut();
-        let max_hp = self.max_hp();
-        *self.hp_mut() = max_hp.min(hp + amount);
-    }
-
-    fn apply_status_effect(&mut self, effect: StatusEffect) {
-        self.status_effects_mut().insert(effect);
-    }
-
-    fn remove_status_effect(&mut self, effect: StatusEffect) {
-        self.status_effects_mut().remove(&effect);
-    }
-
-    fn has_status_effect(&self, effect: StatusEffect) -> bool {
-        self.status_effects().contains(&effect)
-    }
-}
 
 /// All the information the game needs to know about a character
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -105,7 +67,7 @@ pub struct CharStatsStateful {
     // mana starts at 0 each battle
 }
 
-/// Stats that are inherent to a character.
+/// Stats that are inherent to a character or enemy type.
 ///
 /// This doesn't mean they never change; they do every
 /// time you level up or use certain items...
@@ -128,8 +90,7 @@ pub struct InherentStats {
     pub epsilon: IntegralStat, // "the combo one"
 
     // Exclusive to certain characters
-    // NOTE maybe use traits for this?
-    // idk the overhead of dynamic dispatch might not be worth it
+    // Spoiler alert: Ethan is not the only one who needs lambda... :>
     pub lambda: Option<IntegralStat>,
     pub max_mana: Option<IntegralStat>,
 }
