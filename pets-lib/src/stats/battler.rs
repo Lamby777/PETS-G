8f9d0c400a8b43ff57d2bf741c7367f9762b1287
Cherry <cherry@sparklet.org>
2024-03-12 01:17:26 -0400

    split battler trait to another file

---
+++ pets-lib/src/stats/battler.rs
@@ -0,0 +1,41 @@
+use super::*;
+
+/// Trait for stuff that both party members and enemies
+/// have. For example, an enemy doesn't need to have a
+/// "level," but it does need to have HP and status effects.
+trait Battler {
+    fn hp_mut(&mut self) -> &mut IntegralStat;
+    fn max_hp(&self) -> IntegralStat;
+    fn status_effects(&self) -> &HashSet<StatusEffect>;
+    fn status_effects_mut(&mut self) -> &mut HashSet<StatusEffect>;
+    fn inherent_stats(&self) -> &InherentStats;
+
+    /// Subtract damage count from the character's HP
+    ///
+    /// Saturated at 0.
+    fn take_damage(&mut self, damage: IntegralStat) {
+        let hp = *self.hp_mut();
+        *self.hp_mut() = 0.max(hp - damage);
+    }
+
+    /// Add back HP to the character
+    ///
+    /// Saturated at the character's max HP
+    fn heal(&mut self, amount: IntegralStat) {
+        let hp = *self.hp_mut();
+        let max_hp = self.max_hp();
+        *self.hp_mut() = max_hp.min(hp + amount);
+    }
+
+    fn apply_status_effect(&mut self, effect: StatusEffect) {
+        self.status_effects_mut().insert(effect);
+    }
+
+    fn remove_status_effect(&mut self, effect: StatusEffect) {
+        self.status_effects_mut().remove(&effect);
+    }
+
+    fn has_status_effect(&self, effect: StatusEffect) -> bool {
+        self.status_effects().contains(&effect)
+    }
+}
