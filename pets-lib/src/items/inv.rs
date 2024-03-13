95f73e38082d35c12835d82497c34565705ab0a1
Cherry <cherry@sparklet.org>
2024-03-12 22:44:21 -0400

    move `ItemList` trait to `inv.rs`

---
+++ pets-lib/src/items/inv.rs
@@ -0,0 +1,22 @@
+use super::*;
+
+pub trait ItemList {
+    /// Every item that can be equipped
+    fn equipment(&self) -> impl Iterator<Item = &Item>;
+    fn offsets(&self) -> impl Iterator<Item = &InherentStats>;
+}
+
+impl ItemList for &[Item] {
+    fn equipment(&self) -> impl Iterator<Item = &Item> {
+        self.iter().filter(|i| i.is_equipment())
+    }
+
+    fn offsets(&self) -> impl Iterator<Item = &InherentStats> {
+        use ItemCat::*;
+
+        self.iter().filter_map(|i| match &i.category {
+            Equipment(_, offsets) => Some(offsets),
+            _ => None,
+        })
+    }
+}
