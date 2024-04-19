840567216b1d792171eef6c6167909fad9c5f1b6
Cherry <cherry@sparklet.org>
2024-04-19 11:29:21 -0400

    boilerplate for `Choose` trait

---
+++ pets-lib/src/util/choose.rs
@@ -0,0 +1,6 @@
+/// Trait for any custom godot class that lets you pick from a list of choices,
+/// complete with tweening and stuff.
+///
+/// It sounds very specific, but you'd be surprised how often this pattern shows
+/// up in a RPG with lots of dialogue.
+pub trait Choose {}
