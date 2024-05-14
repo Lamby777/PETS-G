4c78cd45e20e787fdf06c59b2f9d288a2527e19f
Cherry <cherry@sparklet.org>
2024-05-13 22:17:38 -0400

    nvm i found a good plugin

--- pets-lib/src/battle/rhythm.rs
+++ pets-lib/src/battle/rhythm.rs
@@ -6,4 +6,27 @@
 //! No fun allowed.
 //!
 
+use derived_deref::*;
 use midly::Smf;
+
+#[derive(Deref, DerefMut)]
+pub struct PetsRhythm<'a>(Smf<'a>);
+
+impl PetsRhythm<'_> {
+    pub fn new(smf: Smf) -> Self {
+        Self(smf)
+    }
+
+    /// Give a time and this function will find you the first
+    /// note before that position in the track.
+    pub fn last_played_note(smf: &Smf, time: u32) -> ! {
+        let beat = beat_track(smf);
+
+        todo!()
+    }
+
+    // getter for finding the beat track out of all the tracks
+    fn beat_track(smf: &Smf) -> ! {
+        todo!()
+    }
+}
