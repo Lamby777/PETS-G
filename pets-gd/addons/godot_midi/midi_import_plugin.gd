24a3a349f3234cc7c0b9bb59a25c03e44b2a7977
Cherry <cherry@sparklet.org>
2024-05-13 22:27:17 -0400

    add `godot_midi` plugin

---
+++ pets-gd/addons/godot_midi/midi_import_plugin.gd
@@ -0,0 +1,61 @@
+@tool
+extends EditorImportPlugin
+
+enum Presets { DEFAULT }
+
+func _get_importer_name():
+	return "com.nlaha.godotmidi"
+
+func _get_visible_name():
+	return "Godot Midi"
+
+func _get_recognized_extensions() -> PackedStringArray:
+	return PackedStringArray(["mid", "midi"])
+
+func _get_save_extension():
+	return "res"
+
+func _get_resource_type():
+	return "MidiResource"
+
+func _get_preset_count():
+	return Presets.size()
+	
+func _get_priority():
+	return 1.0
+	
+func _get_option_visibility(option, name, options):
+	return true
+	
+func _get_import_options(name, preset):
+	match preset:
+		Presets.DEFAULT:
+			return [{
+						"name": "only_notes",
+						"default_value": false
+					}]
+		_:
+			return []
+
+func _get_preset_name(preset):
+	match preset:
+		Presets.DEFAULT:
+			return "Default"
+		_:
+			return "Unknown"
+
+func _get_import_order():
+	return 0
+
+func _import(source_file, save_path, options, r_platform_variants, r_gen_files):
+
+	print("[GodotMidi] Importing midi file: " + source_file)
+	print("[GodotMidi] Importing with only_notes: " + String("yes" if options.only_notes else "no"))
+
+	var save_file = save_path + "." + _get_save_extension()
+	var midi_resource = MidiResource.new()
+	if midi_resource.load_file(source_file) != OK:
+		printerr("[GodotMidi] Failed to load midi file: " + source_file)
+		return FAILED
+
+	return ResourceSaver.save(midi_resource, save_file)
