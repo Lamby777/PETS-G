8451838e9a59c0221584392a29da93f057796662
Cherry <cherry@sparklet.org>
2024-05-25 14:08:07 -0400

    water slows down movement

--- pets-lib/src/world/water_zone.rs
+++ pets-lib/src/world/water_zone.rs
@@ -1,12 +1,14 @@
 use godot::engine::Area2D;
 use godot::prelude::*;
 
+const DEFAULT_WATER_SPEED_MOD: real = 0.4;
+
 #[derive(GodotClass)]
 #[class(init, base=Area2D)]
 pub struct WaterZone {
     base: Base<Area2D>,
 
     #[export]
-    #[init(default = 0.7)]
-    pub speed_modulation: f64,
+    #[init(default = DEFAULT_WATER_SPEED_MOD)]
+    pub speed_modulation: real,
 }
