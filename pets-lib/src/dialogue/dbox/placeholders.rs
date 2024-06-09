db62f16c6ddf8bbd29af13f367cce29748c95584
Cherry <cherry@sparklet.org>
2024-06-09 03:21:29 -0400

    add intro dialogue

--- pets-lib/src/dialogue/dbox/placeholders.rs
+++ pets-lib/src/dialogue/dbox/placeholders.rs
@@ -9,6 +9,10 @@ type DPlaceholder = (&'static str, fn() -> String);
 const PLACEHOLDERS: &[DPlaceholder] = &[
     ("[PLAYER]", || "Cherry".to_string()),
     ("[LEVEL]", || 123.to_string()),
+    // NOTE <https://github.com/Lamby777/PETS-G/issues/23>
+    ("[ETHAN]", || "Ethan".to_string()),
+    ("[MOM]", || "Paula".to_string()),
+    ("[DAD]", || "Clay".to_string()),
 ];
 
 pub fn process_placeholders(text: &str) -> String {
