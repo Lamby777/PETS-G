4456a4d583ed07f5513dadcef39aeaaae04159a8
Cherry <cherry@sparklet.org>
2024-06-09 11:20:33 -0400

    pre enum_map

--- pets-lib/src/battle/affinities.rs
+++ pets-lib/src/battle/affinities.rs
@@ -3,56 +3,26 @@ use godot::prelude::*;
 use super::skills::Element;
 use crate::prelude::*;
 
-/// What kind of damage does the shield block?
-#[derive(Serialize, Deserialize)]
-pub enum Affinities {
-    Physical,
-    Magical,
-    AllElements,
+pub enum AffinityPower {
+    Reflect,
+    Null,
+    Strong,
+    Weak,
+}
 
-    Specific(Vec<Element>),
+#[derive(Serialize, Deserialize)]
+pub struct Affinities {
+    inner: Vec<Element>,
 }
 
 impl Affinities {
-    /// Attempts to convert an explicit list of elements like
-    /// [Blade, Kinetic] into a more general variant like Physical
-    pub fn coerce_specific(&self) -> Option<Self> {
-        use Affinities::*;
-
-        let Specific(elements) = self else {
-            panic!("attempt to `coerce_specific` a non-specific shield");
-        };
-
-        // we don't need to sort the other vectors we're comparing to,
-        // because sorting this one will sort based on enum variant order
-        // and the enum iterators happen to also iterate in order...
-        // (at least i sure hope they do)
-        let mut sorted = elements.clone();
-        sorted.sort();
-
-        Some(if sorted == Element::list_all() {
-            AllElements
-        } else if sorted == Element::list_physical() {
-            Physical
-        } else if sorted == Element::list_magical() {
-            Magical
-        } else {
-            return None;
-        })
+    pub fn describe_shield(&self) -> GString {
+        "Specialized".into()
     }
 }
 
 impl Describe for Affinities {
     fn describe(&self) -> GString {
-        use Affinities::*;
-
-        // TODO use `tr`
-        match self {
-            Specific(_) => "Specialized",
-            AllElements => "Almighty",
-            Magical => "Magical",
-            Physical => "Physical",
-        }
-        .into()
+        todo!()
     }
 }
