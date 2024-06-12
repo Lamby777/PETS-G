473e5723884f54f41c54a9ee418691d8b273575a
Cherry <cherry@sparklet.org>
2024-06-12 01:54:59 -0400

    quests as nodes instead of resources

--- pets-gd/quests/intro1.gd
+++ pets-gd/quests/intro1.gd
@@ -1,1 +1,10 @@
 extends Quest
+
+@onready var zone1 = $"/root/World/YSort/TeleporterDemo/TPRightOut"
+
+func _ready():
+	#zone1.connect("area_entered", self.fulfilled)
+	pass
+
+func _process(delta):
+	pass
