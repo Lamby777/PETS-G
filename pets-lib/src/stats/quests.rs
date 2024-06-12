6891134c2a34794a5f9b176667f7d7917ae00cf7
Cherry <cherry@sparklet.org>
2024-06-12 01:04:40 -0400

    rework quest struct to store phase limit

--- pets-lib/src/stats/quests.rs
+++ pets-lib/src/stats/quests.rs
@@ -6,8 +6,8 @@ pub struct Quests(HashMap<String, Quest>);
 impl Quests {
     pub fn fresh() -> Self {
         let mut this = HashMap::new();
-        let quest = Quest::default();
-        this.insert("INTRO1".to_string(), quest);
+        let quest = Quest::new(12);
+        this.insert("MAIN_STORY".to_string(), quest);
 
         Self(this)
     }
@@ -15,11 +15,23 @@ impl Quests {
 
 #[derive(Serialize, Deserialize)]
 pub struct Quest {
-    pub phase: u8,
+    pub phase: u32,
+    final_phase: u32,
 }
 
-impl Default for Quest {
-    fn default() -> Self {
-        Self { phase: 0 }
+impl Quest {
+    pub fn new(final_phase: u32) -> Self {
+        Self {
+            phase: 0,
+            final_phase,
+        }
+    }
+
+    pub fn final_phase(&self) -> u32 {
+        self.final_phase
+    }
+
+    pub fn is_complete(&self) -> bool {
+        self.phase >= self.final_phase
     }
 }
