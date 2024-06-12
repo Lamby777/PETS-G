e4049fd65a831f1dbb268768c56413a6397bcc9b
Cherry <cherry@sparklet.org>
2024-06-12 00:47:13 -0400

    added `Quests` wrapper type + quest phases

--- pets-lib/src/stats/quests.rs
+++ pets-lib/src/stats/quests.rs
@@ -1,12 +1,25 @@
 use crate::prelude::*;
 
-#[derive(Serialize, Deserialize)]
-pub struct Quest {
-    pub id: String,
-    pub requirements: Vec<Box<dyn Requirement>>,
+#[derive(Deref, DerefMut, Serialize, Deserialize)]
+pub struct Quests(HashMap<String, Quest>);
+
+impl Quests {
+    pub fn fresh() -> Self {
+        let mut this = HashMap::new();
+        let quest = Quest::default();
+        this.insert("INTRO1".to_string(), quest);
+
+        Self(this)
+    }
 }
 
-#[typetag::serde(tag = "type")]
-pub trait Requirement {
-    fn fulfilled(&self) -> bool;
+#[derive(Serialize, Deserialize)]
+pub struct Quest {
+    pub phase: u8,
+}
+
+impl Default for Quest {
+    fn default() -> Self {
+        Self { phase: 0 }
+    }
 }
