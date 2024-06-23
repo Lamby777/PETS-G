27c81d0dc9d339f77748e3094ce631c4ead54790
Cherry <cherry@sparklet.org>
2024-06-23 16:34:13 -0400

    change parent names to Mom and Dad if party leader is Ethan

--- pets-lib/src/dialogue/dbox/placeholders.rs
+++ pets-lib/src/dialogue/dbox/placeholders.rs
@@ -2,14 +2,35 @@
 //! This module is for processing placeholders in dialogue strings.
 //!
 
+use crate::prelude::*;
+
+pub fn is_party_leader(pchar: &PChar) -> bool {
+    let party = PlayerCB::singleton().bind().party_pchars();
+    let leader = party.first().unwrap();
+
+    dbg!(leader, pchar);
+    leader == pchar
+}
+
 const PLACEHOLDERS: &[(&'static str, fn() -> String)] = &[
     ("[PLAYER]", || "Cherry".to_string()),
     ("[LEVEL]", || 123.to_string()),
     // NOTE <https://github.com/Lamby777/PETS-G/issues/23>
     ("[ETHAN]", || "Ethan".to_string()),
-    // TODO show "Mom" and "Dad" if the party leader is Ethan, else show their names
-    ("[MOM]", || "Paula (Mom)".to_string()),
-    ("[DAD]", || "Clay (Dad)".to_string()),
+    ("[MOM]", || {
+        match is_party_leader(&PChar::ETHAN) {
+            true => "Mom",
+            false => "Paula",
+        }
+        .to_string()
+    }),
+    ("[DAD]", || {
+        match is_party_leader(&PChar::ETHAN) {
+            true => "Dad",
+            false => "Clay",
+        }
+        .to_string()
+    }),
 ];
 
 pub fn process_placeholders(text: &str) -> String {
