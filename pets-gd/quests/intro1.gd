dab003938588a1d83aef619e8b964978e9f12974
Cherry <cherry@sparklet.org>
2024-06-24 22:56:10 -0400

    quest room stuff reworked

--- pets-gd/quests/intro1.gd
+++ pets-gd/quests/intro1.gd
@@ -1,21 +1,44 @@
 extends Quest
 
-@onready var zone1 = $"../../YSort/Room/HouseEntrance"
 @onready var dbox = DialogBox.singleton()
+@onready var pcb = PlayerCB.singleton()
 
-func on_zone1_enter(_body):
-    zone1.disconnect("body_entered", on_zone1_enter)
-    
-    # TODO make a signal for when a transition is over
-    await PlayerCB.singleton().teleported
-
-    dbox.start_ix("Intro #2")
-    
-    # connect some other zone's `body_entered` signal
+func room_id():
+    return $"../../YSort/Room".room_id
 
 func _ready():
     dbox.start_ix("Intro #1")
-    zone1.connect("body_entered", on_zone1_enter)
-    
-func _process(delta):
-    pass
+    pcb.teleported.connect(on_teleported)
+
+    on_world_tp()
+
+func on_teleported(target):
+    # It needs to know which room you're teleporting inside, because
+    # the rooms have different teleporters that may or may not exist
+    if room_id() == "CV_Outdoors":
+        on_world_tp()
+    elif room_id() == "CV_Houses":
+        on_house_tp(target)
+
+func on_world_tp():
+    await pcb.teleported
+
+    $"../../YSort/Room/HouseEntrance".body_entered.connect(on_world_tp)
+
+    if phase < 1:
+        self.phase = 1
+        dbox.start_ix("Intro #2")
+
+
+func on_house_tp(target):
+    await pcb.teleported
+
+    $"../../YSort/Room/EthanBedroomEntrance".body_entered.connect(on_house_tp)
+
+    if phase < 2:
+        self.phase = 2
+        dbox.start_ix("Intro #2")
+
+    if target.name == "EthanBedroomEntrance" and phase < 3:
+        self.phase = 3
+        dbox.start_ix("Intro #3")
