8116efe9f8dfc1a866974fdce0078a806085f68b
Cherry <cherry@sparklet.org>
2024-06-26 23:44:46 -0400

    fix allowing movement disabling cs motions

--- pets-gd/quests/intro1.gd
+++ pets-gd/quests/intro1.gd
@@ -4,6 +4,11 @@ func room_id():
     return $"../../YSort/Room".room_id
 
 func _ready():
+    pcb().move_to_relative(0.0, -200.0)
+    print("moving")
+    await pcb().motion_done
+    print("done")
+
     dbox().start_ix("Intro #1")
     pcb().teleported.connect(on_teleported)
 
