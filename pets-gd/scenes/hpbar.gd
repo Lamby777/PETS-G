86ad5f52ad745a2c5d11d5bd5b573fd7dcb0e80c
Cherry <cherry@sparklet.org>
2024-06-29 19:48:20 -0400

    use tool script for stat bars

---
+++ pets-gd/scenes/hpbar.gd
@@ -0,0 +1,19 @@
+@tool
+class_name StatBar
+extends ProgressBar
+
+@onready var bar_label = $Label
+@onready var value_label = $Value
+
+@export var bar_label_text: String:
+    get:
+        return bar_label.text
+    set(val):
+        bar_label.text = val
+
+@export var bar_value: int:
+    get:
+        return value
+    set(val):
+        value = val
+        value_label.text = "[right]" + str(val)
