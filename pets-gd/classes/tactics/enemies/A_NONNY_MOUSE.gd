b519756ed1cf479cfd058265af5f7964b808441d
Cherry <cherry@sparklet.org>
2024-07-04 00:00:53 -0400

    randomize mouse1 bullets

--- pets-gd/classes/tactics/enemies/A_NONNY_MOUSE.gd
+++ pets-gd/classes/tactics/enemies/A_NONNY_MOUSE.gd
@@ -1,16 +1,20 @@
 extends Tactics
 
-@export var bullet: PackedScene
+@export var bullet_scene: PackedScene
 
 func _start():
-    print("Start")
     super()
     timer.one_shot = false
-    timer.wait_time = 1
+    timer.wait_time = 0.6
     timer.start()
 
 func _attack():
-    print("Attack!")
-    var bullet = bullet.instantiate()
-    bullet.icon = %BattleIcon
-    %Board.add_child(bullet)
+    var viewport_size = get_viewport().get_visible_rect().size
+
+    for n in range(2):
+        var bullet = bullet_scene.instantiate()
+        bullet.icon = %BattleIcon
+        bullet.position.x = n * viewport_size.x
+        bullet.position.y = randi() % int(viewport_size.y)
+        bullet.speed = 300 + (randi() % 200)
+        %Board.add_child(bullet)
