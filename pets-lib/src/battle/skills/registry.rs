8e926061518cbc7aa1ecf246a180733f865b66f4
Cherry <cherry@sparklet.org>
2024-07-15 14:49:36 -0400

    item registry uses `registry.rs`

--- pets-lib/src/battle/skills/registry.rs
+++ pets-lib/src/battle/skills/registry.rs
@@ -15,12 +15,7 @@ pub static SKILL_REGISTRY: OnceLock<HashMap<String, Box<dyn Skill>>> =
 /// Initializes `SKILL_REGISTRY` by scanning for vanilla and
 /// modded skill registries and combining the list of skills.
 pub fn load_skill_registry() {
-    godot_print!("Reading vanilla skill registries.");
     let mut skills = find_vanilla("skillregistries");
-
-    godot_print!("Reading modded skill registries.");
     skills.extend(find_modded("skills"));
-
-    godot_print!("Finished reading skill registries.\n\n");
     SKILL_REGISTRY.set(skills).unwrap();
 }
