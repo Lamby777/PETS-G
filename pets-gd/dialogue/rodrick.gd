d3f4d340344b4c4579617c04fbdb4c4dd126ca23
Cherry <cherry@sparklet.org>
2024-08-17 18:25:08 -0400

    made 2-page rodrick sign

--- pets-gd/dialogue/rodrick.gd
+++ pets-gd/dialogue/rodrick.gd
@@ -5,4 +5,14 @@ func _start() -> void:
     dbox().set_speaker("[RODRICK]")
     dbox().say("DG_RODRICK1_SOYOURE")
     await dbox().accept
+    dbox().say("DG_RODRICK1_AREYOUSMART")
+    await dbox().accept
+
+    # > DG_RODRICK1_NOPE
+    # DG_RODRICK1_DIDNT
+    # > DG_RODRICK1_DEFNOT
+    # DG_RODRICK1_DEFDIDNT
+    #
+    # DG_RODRICK1_COMEBACK
+
     dbox().close()
