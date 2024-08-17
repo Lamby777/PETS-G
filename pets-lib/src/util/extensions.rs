d6b6b9bfd7b4b27b562f366ce7f719e7508d7ab9
Cherry <cherry@sparklet.org>
2024-08-17 01:28:53 -0400

    rm `Vector2Ext` (`to_tuple` got added!!!)

--- pets-lib/src/util/extensions.rs
+++ pets-lib/src/util/extensions.rs
@@ -1,17 +1,6 @@
 use godot::classes::AnimationPlayer;
 use godot::prelude::*;
 
-pub trait Vector2Ext {
-    fn to_tuple(&self) -> (f32, f32);
-}
-
-impl Vector2Ext for Vector2 {
-    /// Convert the godot Vector2 into a tuple of x and y.
-    fn to_tuple(&self) -> (f32, f32) {
-        (self.x, self.y)
-    }
-}
-
 pub trait AnimationPlayerExt {
     fn play_animation_forwards(
         &mut self,
