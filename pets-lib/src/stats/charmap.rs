c3f4297b384d880fe3362501a9f51bfb9f673b3b
Cherry <cherry@sparklet.org>
2024-08-19 14:28:20 -0400

    better charmap debugging

--- pets-lib/src/stats/charmap.rs
+++ pets-lib/src/stats/charmap.rs
@@ -17,16 +17,26 @@ impl CharMap {
             .expect("Character not found")
             .clone()
     }
+
+    /// For testing serialization issues
+    pub fn _debugging_charmap() -> CharMap {
+        CharMap(vec![Rc::new(RefCell::new(CharData::default()))])
+    }
 }
 
 /// CharMap at the start of the game
 /// Most characters have unique base stats
 pub fn default_charmap() -> CharMap {
+    godot_print!("Loading default `charmap.json`");
     let mut file =
         GFile::open(CHARMAP_PATH, ModeFlags::READ).expect("charmap not found");
 
     let mut content = vec![];
     file.read_to_end(&mut content).unwrap();
     let content = String::from_utf8(content).unwrap();
-    serde_json::from_str(&content).expect("deserialization of charmap failed")
+    let res = serde_json::from_str(&content)
+        .expect("deserialization of charmap failed");
+
+    godot_print!("Loaded default `charmap.json`");
+    res
 }
