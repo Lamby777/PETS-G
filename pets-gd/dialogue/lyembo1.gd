384ebb5af8355b5518f5c440c9713af2a06f5dea
Cherry <cherry@sparklet.org>
2024-08-20 23:10:59 -0400

    rm new lyembo interaction segment

--- pets-gd/dialogue/lyembo1.gd
+++ pets-gd/dialogue/lyembo1.gd
@@ -6,11 +6,3 @@ func _start() -> void:
         "I don't know what to say.",
         "My name is L'yembo. Maybe we'll meet again someday.",
     ])
-
-    var npcb = get_parent().get_parent()
-    npcb.move_to_relative(-200.0, 200.0)
-    await npcb.motion_done
-
-    await dbox().say_as("[LYEMBO]", [
-        "Motion done.",
-    ])
