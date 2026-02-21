afbe7506bb48029d795fbe2b6ad2813e11c480d7
Cherry <cherry@sparklet.org>
2026-02-21 18:38:10 -0500

    not everyone uses the same project structure as you holy shit (refactor later maybe)

--- grcc.mk
+++ grcc.mk
@@ -153,7 +153,7 @@ GRCC_NATIVE_DEBUG_WINDOWS_SRC=./rust/target/debug/$(GRCC_GODOT_RUST_LIB_NAME).dl
 GRCC_NATIVE_DEBUG_MACOSX_SRC=./rust/target/debug/lib$(GRCC_GODOT_RUST_LIB_NAME).dylib
 GRCC_NATIVE_DEBUG_LINUX_SRC=./rust/target/debug/lib$(GRCC_GODOT_RUST_LIB_NAME).so
 
-GRCC_GODOT_GDNATIVE_DIR=./godot/gdnative
+GRCC_GODOT_GDNATIVE_DIR=./pets-gd/gdnative
 GRCC_WINDOWS_X64_SRC=./rust/target/$(GRCC_WINDOWS_X64_TARGET)/release/$(GRCC_GODOT_RUST_LIB_NAME).dll
 GRCC_WINDOWS_X64_DST=$(GRCC_GODOT_GDNATIVE_DIR)/windows/$(GRCC_WINDOWS_X64_TARGET)/
 GRCC_WINDOWS_ARM64_SRC=./rust/target/$(GRCC_WINDOWS_ARM64_TARGET)/release/$(GRCC_GODOT_RUST_LIB_NAME).dll
@@ -207,23 +207,23 @@ GRCC_DMG_VOLUME_NAME=$(GRCC_GAME_PKG_NAME)
 GRCC_GODOT_HEADLESS=godot --headless
 
 grcc-test:
-	cd rust && cargo test
+	cd pets-lib && cargo test
 
 grcc-debug:
-	cd rust && cargo build
+	cd pets-lib && cargo build
 
 grcc-release:
-	cd rust && cargo build --release
+	cd pets-lib && cargo build --release
 
 grcc-clean: grcc-clean-prepare
 	rm -rf export
 
 grcc-doc:
-	cd rust && cargo doc --workspace --offline
+	cd pets-lib && cargo doc --workspace --offline
 
 grcc-clean-prepare:
 	rm -rf $(GRCC_GODOT_GDNATIVE_DIR)
-	cd rust && (cargo clean || rm -rf ./target || sudo rm -rf ./target)
+	cd pets-lib && (cargo clean || rm -rf ./target || sudo rm -rf ./target)
 
 grcc-lib-all: grcc-lib-windows grcc-lib-android grcc-lib-macosx grcc-lib-linux
 
@@ -231,85 +231,85 @@ grcc-lib-windows: grcc-lib-windows-x64 grcc-lib-windows-arm64
 
 grcc-lib-windows-x64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) -e C_INCLUDE_PATH=$(GRCC_WINDOWS_MINGW_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WINDOWS_X64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) -e C_INCLUDE_PATH=$(GRCC_WINDOWS_MINGW_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WINDOWS_X64_TARGET)
 else
-	export C_INCLUDE_PATH=$(GRCC_WINDOWS_MINGW_HEADERS) && cd rust && cargo build --release --target $(GRCC_WINDOWS_X64_TARGET)
+	export C_INCLUDE_PATH=$(GRCC_WINDOWS_MINGW_HEADERS) && cd pets-lib && cargo build --release --target $(GRCC_WINDOWS_X64_TARGET)
 endif
 
 grcc-lib-windows-arm64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WINDOWS_ARM64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WINDOWS_ARM64_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_WINDOWS_ARM64_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_WINDOWS_ARM64_TARGET)
 endif
 
 grcc-lib-android: grcc-check-android-host grcc-lib-android-arm64 grcc-lib-android-arm32 grcc-lib-android-x64 grcc-lib-android-x32
 
 grcc-lib-android-arm64: grcc-check-android-host
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_ARM64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_ARM64_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_ANDROID_ARM64_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_ANDROID_ARM64_TARGET)
 endif
 
 grcc-lib-android-arm32: grcc-check-android-host
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_ARM32_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_ARM32_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_ANDROID_ARM32_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_ANDROID_ARM32_TARGET)
 endif
 
 grcc-lib-android-x64: grcc-check-android-host
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_X64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_X64_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_ANDROID_X64_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_ANDROID_X64_TARGET)
 endif
 
 grcc-lib-android-x32: grcc-check-android-host
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_X32_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_ANDROID_X32_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_ANDROID_X32_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_ANDROID_X32_TARGET)
 endif
 
 grcc-lib-macosx: grcc-lib-macosx-x64 grcc-lib-macosx-arm64
 
 grcc-lib-macosx-x64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) -e CC=$(GRCC_MACOSX_SDK_CC_X64) -e C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_MACOSX_X64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) -e CC=$(GRCC_MACOSX_SDK_CC_X64) -e C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_MACOSX_X64_TARGET)
 else
-	export CC=$(GRCC_MACOSX_SDK_CC_X64) C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) && cd rust && cargo build --release --target $(GRCC_MACOSX_X64_TARGET)
+	export CC=$(GRCC_MACOSX_SDK_CC_X64) C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) && cd pets-lib && cargo build --release --target $(GRCC_MACOSX_X64_TARGET)
 endif
 
 grcc-lib-macosx-arm64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) -e CC=$(GRCC_MACOSX_SDK_CC_ARM64) -e C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_MACOSX_ARM64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) -e CC=$(GRCC_MACOSX_SDK_CC_ARM64) -e C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_MACOSX_ARM64_TARGET)
 else
-	export CC=$(GRCC_MACOSX_SDK_CC_ARM64) C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) && cd rust && cargo build --release --target $(GRCC_MACOSX_ARM64_TARGET)
+	export CC=$(GRCC_MACOSX_SDK_CC_ARM64) C_INCLUDE_PATH=$(GRCC_MACOSX_SDK_HEADERS) && cd pets-lib && cargo build --release --target $(GRCC_MACOSX_ARM64_TARGET)
 endif
 
 grcc-lib-linux: grcc-lib-linux-x64 grcc-lib-linux-arm64
 
 grcc-lib-linux-x64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_LINUX_X64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_LINUX_X64_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_LINUX_X64_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_LINUX_X64_TARGET)
 endif
 
 grcc-lib-linux-arm64:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_LINUX_ARM64_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_LINUX_ARM64_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_LINUX_ARM64_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_LINUX_ARM64_TARGET)
 endif
 
 grcc-lib-wasm:
 ifeq (yes,$(GRCC_USE_DOCKER))
-	cd rust && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WASM_TARGET)
+	cd pets-lib && $(GRCC_INVOKE_DOCKER_RUST) ufoot/godot-rust-cross-compiler cargo build --release --target $(GRCC_WASM_TARGET)
 else
-	cd rust && cargo build --release --target $(GRCC_WASM_TARGET)
+	cd pets-lib && cargo build --release --target $(GRCC_WASM_TARGET)
 endif
 	# Optimize WASM with wasm-opt
 ifeq (yes,$(GRCC_USE_DOCKER))
@@ -376,7 +376,7 @@ grcc-copy-wasm: grcc-lib-wasm
 grcc-pkg-all: grcc-pkg-windows grcc-pkg-android grcc-pkg-macosx grcc-pkg-linux grcc-pkg-wasm grcc-pkg-source
 
 # [TODO] report this bug, need to launch the export twice for it to work, else complains about missing lib
-GRCC_PKG_BUILDX2=godot/buildx2.sh
+GRCC_PKG_BUILDX2=pets-gd/buildx2.sh
 
 # Godot 4 export preset names (architecture-specific presets must be defined in export_presets.cfg)
 GRCC_EXPORT_PRESET_WINDOWS_X64=Windows Desktop x64
@@ -390,61 +390,61 @@ GRCC_EXPORT_PRESET_WEB=Web
 grcc-pkg-windows: grcc-pkg-windows-x64 grcc-pkg-windows-arm64
 
 grcc-pkg-windows-x64: grcc-copy-windows-x64
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_X64_PKG).zip godot/$(GRCC_GAME_PKG_NAME).exe godot/$(GRCC_GODOT_RUST_LIB_NAME).dll
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_WINDOWS_X64)" $(GRCC_GAME_PKG_NAME).exe ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_X64_PKG).zip pets-gd/$(GRCC_GAME_PKG_NAME).exe pets-gd/$(GRCC_GODOT_RUST_LIB_NAME).dll
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_WINDOWS_X64)" $(GRCC_GAME_PKG_NAME).exe ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
 	install -d $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_X64_PKG)
-	mv godot/$(GRCC_GAME_PKG_NAME).exe godot/$(GRCC_GODOT_RUST_LIB_NAME).dll $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_X64_PKG)
+	mv pets-gd/$(GRCC_GAME_PKG_NAME).exe pets-gd/$(GRCC_GODOT_RUST_LIB_NAME).dll $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_X64_PKG)
 	cd $(GRCC_EXPORT_DIR) && zip -r $(GRCC_EXPORT_WINDOWS_X64_PKG).zip $(GRCC_EXPORT_WINDOWS_X64_PKG) && rm -rf $(GRCC_EXPORT_WINDOWS_X64_PKG)
 
 grcc-pkg-windows-arm64: grcc-copy-windows-arm64
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_ARM64_PKG).zip godot/$(GRCC_GAME_PKG_NAME).exe godot/$(GRCC_GODOT_RUST_LIB_NAME).dll
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_WINDOWS_ARM64)" $(GRCC_GAME_PKG_NAME).exe ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_ARM64_PKG).zip pets-gd/$(GRCC_GAME_PKG_NAME).exe pets-gd/$(GRCC_GODOT_RUST_LIB_NAME).dll
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_WINDOWS_ARM64)" $(GRCC_GAME_PKG_NAME).exe ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
 	install -d $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_ARM64_PKG)
-	mv godot/$(GRCC_GAME_PKG_NAME).exe godot/$(GRCC_GODOT_RUST_LIB_NAME).dll $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_ARM64_PKG)
+	mv pets-gd/$(GRCC_GAME_PKG_NAME).exe pets-gd/$(GRCC_GODOT_RUST_LIB_NAME).dll $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WINDOWS_ARM64_PKG)
 	cd $(GRCC_EXPORT_DIR) && zip -r $(GRCC_EXPORT_WINDOWS_ARM64_PKG).zip $(GRCC_EXPORT_WINDOWS_ARM64_PKG) && rm -rf $(GRCC_EXPORT_WINDOWS_ARM64_PKG)
 
 grcc-pkg-android: grcc-check-android-host grcc-copy-android
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_ANDROID_PKG).apk godot/$(GRCC_EXPORT_ANDROID_PKG).apk
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_ANDROID)" $(GRCC_EXPORT_ANDROID_PKG).apk ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
-	install -d $(GRCC_EXPORT_DIR) && mv godot/$(GRCC_EXPORT_ANDROID_PKG).apk $(GRCC_EXPORT_DIR)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_ANDROID_PKG).apk pets-gd/$(GRCC_EXPORT_ANDROID_PKG).apk
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_ANDROID)" $(GRCC_EXPORT_ANDROID_PKG).apk ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	install -d $(GRCC_EXPORT_DIR) && mv pets-gd/$(GRCC_EXPORT_ANDROID_PKG).apk $(GRCC_EXPORT_DIR)
 
 grcc-pkg-macosx: grcc-copy-macosx
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_MACOSX_PKG).zip godot/$(GRCC_EXPORT_MACOSX_PKG).zip
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_MACOSX)" $(GRCC_EXPORT_MACOSX_PKG).zip ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
-	install -d $(GRCC_EXPORT_DIR) && mv godot/$(GRCC_EXPORT_MACOSX_PKG).zip $(GRCC_EXPORT_DIR)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_MACOSX_PKG).zip pets-gd/$(GRCC_EXPORT_MACOSX_PKG).zip
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_MACOSX)" $(GRCC_EXPORT_MACOSX_PKG).zip ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	install -d $(GRCC_EXPORT_DIR) && mv pets-gd/$(GRCC_EXPORT_MACOSX_PKG).zip $(GRCC_EXPORT_DIR)
 
 grcc-pkg-linux: grcc-pkg-linux-x64 grcc-pkg-linux-arm64
 
 grcc-pkg-linux-x64: grcc-copy-linux-x64
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_X64_PKG).tar.gz godot/$(GRCC_GAME_PKG_NAME) godot/lib$(GRCC_GODOT_RUST_LIB_NAME).so
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_LINUX_X64)" $(GRCC_GAME_PKG_NAME) ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_X64_PKG).tar.gz pets-gd/$(GRCC_GAME_PKG_NAME) pets-gd/lib$(GRCC_GODOT_RUST_LIB_NAME).so
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_LINUX_X64)" $(GRCC_GAME_PKG_NAME) ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
 	install -d $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_X64_PKG)
-	mv godot/$(GRCC_GAME_PKG_NAME) godot/lib$(GRCC_GODOT_RUST_LIB_NAME).so $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_X64_PKG)
+	mv pets-gd/$(GRCC_GAME_PKG_NAME) pets-gd/lib$(GRCC_GODOT_RUST_LIB_NAME).so $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_X64_PKG)
 	cd $(GRCC_EXPORT_DIR) && tar czf $(GRCC_EXPORT_LINUX_X64_PKG).tar.gz $(GRCC_EXPORT_LINUX_X64_PKG) && rm -rf $(GRCC_EXPORT_LINUX_X64_PKG)
 
 grcc-pkg-linux-arm64: grcc-copy-linux-arm64
-	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_ARM64_PKG).tar.gz godot/$(GRCC_GAME_PKG_NAME) godot/lib$(GRCC_GODOT_RUST_LIB_NAME).so
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_LINUX_ARM64)" $(GRCC_GAME_PKG_NAME) ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	rm -f $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_ARM64_PKG).tar.gz pets-gd/$(GRCC_GAME_PKG_NAME) pets-gd/lib$(GRCC_GODOT_RUST_LIB_NAME).so
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_LINUX_ARM64)" $(GRCC_GAME_PKG_NAME) ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
 	install -d $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_ARM64_PKG)
-	mv godot/$(GRCC_GAME_PKG_NAME) godot/lib$(GRCC_GODOT_RUST_LIB_NAME).so $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_ARM64_PKG)
+	mv pets-gd/$(GRCC_GAME_PKG_NAME) pets-gd/lib$(GRCC_GODOT_RUST_LIB_NAME).so $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_LINUX_ARM64_PKG)
 	cd $(GRCC_EXPORT_DIR) && tar czf $(GRCC_EXPORT_LINUX_ARM64_PKG).tar.gz $(GRCC_EXPORT_LINUX_ARM64_PKG) && rm -rf $(GRCC_EXPORT_LINUX_ARM64_PKG)
 
 grcc-pkg-wasm: grcc-copy-wasm
 	rm -rf $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WASM_PKG) $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WASM_PKG).zip
 	install -d $(GRCC_EXPORT_DIR)/$(GRCC_EXPORT_WASM_PKG)
-	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path godot --export-release "$(GRCC_EXPORT_PRESET_WEB)" $(GRCC_EXPORT_WASM_PKG)/index.html ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
-	mv godot/$(GRCC_EXPORT_WASM_PKG) $(GRCC_EXPORT_DIR)/
+	echo 'for i in warmup real ; do $(GRCC_GODOT_HEADLESS) --path pets-gd --export-release "$(GRCC_EXPORT_PRESET_WEB)" $(GRCC_EXPORT_WASM_PKG)/index.html ; done' > $(GRCC_PKG_BUILDX2) && chmod a+x $(GRCC_PKG_BUILDX2) && $(GRCC_INVOKE_DOCKER_GODOT_EXPORT) sh $(GRCC_PKG_BUILDX2) && rm $(GRCC_PKG_BUILDX2)
+	mv pets-gd/$(GRCC_EXPORT_WASM_PKG) $(GRCC_EXPORT_DIR)/
 	cd $(GRCC_EXPORT_DIR) && zip -r $(GRCC_EXPORT_WASM_PKG).zip $(GRCC_EXPORT_WASM_PKG) && rm -rf $(GRCC_EXPORT_WASM_PKG)
 
 grcc-pkg-source: .git/config grcc-clean-prepare
-	export REPO="$$(grep url .git/config | head -n 1 | cut -d = -f 2)" && install -d $(GRCC_EXPORT_DIR) && rm -f $(GRCC_EXPORT_DIR)/$(GRCC_GAME_REPO_NAME).tar && tar cf $(GRCC_EXPORT_DIR)/$(GRCC_GAME_REPO_NAME).tar --exclude=.git --exclude=export --exclude=rust/target --exclude=godot/.godot . && cd $(GRCC_EXPORT_DIR) && rm -rf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && rm -f $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).tar.gz $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).zip && mkdir $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && cd $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && tar xf ../$(GRCC_GAME_REPO_NAME).tar && cd .. && rm $(GRCC_GAME_REPO_NAME).tar && tar czf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).tar.gz $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && zip -r $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).zip $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && rm -rf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION)
+	export REPO="$$(grep url .git/config | head -n 1 | cut -d = -f 2)" && install -d $(GRCC_EXPORT_DIR) && rm -f $(GRCC_EXPORT_DIR)/$(GRCC_GAME_REPO_NAME).tar && tar cf $(GRCC_EXPORT_DIR)/$(GRCC_GAME_REPO_NAME).tar --exclude=.git --exclude=export --exclude=rust/target --exclude=pets-gd/.godot . && cd $(GRCC_EXPORT_DIR) && rm -rf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && rm -f $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).tar.gz $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).zip && mkdir $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && cd $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && tar xf ../$(GRCC_GAME_REPO_NAME).tar && cd .. && rm $(GRCC_GAME_REPO_NAME).tar && tar czf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).tar.gz $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && zip -r $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION).zip $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION) && rm -rf $(GRCC_GAME_REPO_NAME)-$(GRCC_GAME_REPO_VERSION)
 
 # Windows installers (NSIS)
 # -------------------------
 
 grcc-installer-windows: grcc-installer-windows-x64 grcc-installer-windows-arm64
 
-GRCC_INSTALLER_BUILDSCRIPT=godot/installer-build.sh
+GRCC_INSTALLER_BUILDSCRIPT=pets-gd/installer-build.sh
 
 grcc-installer-windows-x64: grcc-pkg-windows-x64
 	install -d $(GRCC_EXPORT_DIR)
@@ -479,7 +479,7 @@ grcc-installer-windows-arm64: grcc-pkg-windows-arm64
 # Creates a DMG from the macOS zip export
 # Uses genisoimage to create hybrid ISO and libdmg-hfsplus to convert to DMG
 
-GRCC_DMG_BUILDSCRIPT=godot/dmg-build.sh
+GRCC_DMG_BUILDSCRIPT=pets-gd/dmg-build.sh
 
 grcc-dmg-macosx: grcc-pkg-macosx
 	install -d $(GRCC_EXPORT_DIR)
