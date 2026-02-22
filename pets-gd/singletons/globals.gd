68af25084b6c2bd0e21cd31956fec277f530ee9d
Cherry <cherry@sparklet.org>
2026-02-22 14:02:24 -0500

    switch to updated 4.1 discord rpc plugin (prints many warnings and leaks...)

--- pets-gd/singletons/globals.gd
+++ pets-gd/singletons/globals.gd
@@ -11,18 +11,18 @@ func _ready():
     discord_rich_presence()
 
 func discord_rich_presence():
-    discord_sdk.app_id = 1115759778644365353
-    print("Discord working: " + str(discord_sdk.get_is_discord_working()))
-    discord_sdk.details = "Epic Gaming"
-    discord_sdk.state = "@ Corr Valley"
+    DiscordRPC.app_id = 1115759778644365353
+    print("Discord working: " + str(DiscordRPC.get_is_discord_working()))
+    DiscordRPC.details = "Epic Gaming"
+    DiscordRPC.state = "@ Corr Valley"
     
-    discord_sdk.large_image = "mira-what"
-    discord_sdk.large_image_text = "Epic Gaming, indeed."
-    discord_sdk.small_image = "mira-what"
-    discord_sdk.small_image_text = "Catchin' Zs."
+    DiscordRPC.large_image = "mira-what"
+    DiscordRPC.large_image_text = "Epic Gaming, indeed."
+    DiscordRPC.small_image = "mira-what"
+    DiscordRPC.small_image_text = "Catchin' Zs."
 
-    discord_sdk.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
+    DiscordRPC.start_timestamp = int(Time.get_unix_time_from_system()) # "02:46 elapsed"
     # discord_sdk.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time / "01:00 remaining"
 
     # Always refresh after changing the values!
-    discord_sdk.refresh()
+    DiscordRPC.refresh()
