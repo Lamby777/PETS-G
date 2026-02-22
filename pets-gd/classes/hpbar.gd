4ab12cf3e8e3fafa14aa75148f01cea15f0ef04b
Cherry <cherry@sparklet.org>
2026-02-21 21:56:43 -0500

    add back hpbar null checks (annoying errors on editor startup)

--- pets-gd/classes/hpbar.gd
+++ pets-gd/classes/hpbar.gd
@@ -9,11 +9,13 @@ extends ProgressBar
     get:
         return bar_label.text
     set(val):
-        bar_label.text = val
+        if bar_label:
+            bar_label.text = val
 
 @export var bar_value: float:
     get:
         return value
     set(val):
         value = val
-        value_label.text = str(val)
+        if value_label:
+            value_label.text = str(val)
